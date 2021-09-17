a9b9a765bd84e7f4ae57da3412c34bf4f4f1566d
Samuel Thompson <sam.thompson356@gmail.com>
2021-09-17 18:17:03 -0400

    remove magic values from server

--- index.js
+++ index.js
@@ -10,21 +10,31 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
+        methods: ["GET"]
     }
 });
 
 const AVATAR_SPEED = SETTINGS.gameSettings.avatarSpeed; //pixels per second
 const FRAMES_PER_SECOND = SETTINGS.gameSettings.framesPerSecond;
+const MILLIS_PER_FRAME = 1000 / FRAMES_PER_SECOND;
 const BOUNDS = {
     MIN_X: SETTINGS.gameSettings.bounds.minX,
     MAX_X: SETTINGS.gameSettings.bounds.maxX,
     MIN_Y: SETTINGS.gameSettings.bounds.minY,
     MAX_Y: SETTINGS.gameSettings.bounds.maxY
 };
+const VERBOSITY = SETTINGS.debugSettings.verbosity;
+const AVATAR_START_POS = SETTINGS.gameSettings.avatarStartPos;
 
 const clientList = [];
 var clientCount = 0;
+
+function logDebugMessage(message, verbosityLevel) {
+    if (VERBOSITY < verbosityLevel) {
+        return;
+    }
+    console.log(message);
+}
 
 app.use(express.static(path.join(__dirname, 'client/build')))
 
@@ -42,13 +52,13 @@ app.get('/api/players', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    console.log("Received a GET request.");
+    logDebugMessage("Received a GET request.", 1);
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 io.on('connection', (socket) => {
     const clientId = clientCount;
-    console.log(`A user with id ${clientId} connected.`);
+    logDebugMessage(`A user with id ${clientId} connected.`, 1);
     const newClient = { 
         id: clientId, 
         name: null,
@@ -68,7 +78,7 @@ io.on('connection', (socket) => {
     socket.on('connect_avatar', (name, color) => {
         newClient.name = name;
         newClient.color = color;
-        newClient.avatar = new ava.Avatar(250, 100, color);
+        newClient.avatar = new ava.Avatar(AVATAR_START_POS.x, AVATAR_START_POS.y, color);
         for (let otherClient of clientList) {
             otherClient.socket.emit('avatar_joined', newClient.id);
         }
@@ -84,7 +94,7 @@ io.on('connection', (socket) => {
             up: false,
             down: false,
         };
-        console.log(`${newClient.id}'s avatar has left.`);
+        logDebugMessage(`${newClient.id}'s avatar has left.`, 1);
         for (let otherClient of clientList) {
             otherClient.socket.emit('avatar_left', newClient.id);
         }
@@ -104,7 +114,7 @@ io.on('connection', (socket) => {
         roomHeight: BOUNDS.MAX_Y - BOUNDS.MIN_Y
     });
     socket.on('disconnect', () => {
-        console.log("A user disconnected.");
+        logDebugMessage("A user disconnected.", 1);
         disconnector_idx = clientList.findIndex((client) => client.id === clientId);
         clientList.splice(disconnector_idx, 1);
         for (let client of clientList) {
@@ -146,6 +156,6 @@ function gameLoop() {
 }
 
 server.listen(PORT, () => {
-    console.log(`server listening on port ${PORT}`);
-    setInterval(gameLoop, 1000 / FRAMES_PER_SECOND);
+    logDebugMessage(`server listening on port ${PORT}`, 1);
+    setInterval(gameLoop, MILLIS_PER_FRAME);
 });
